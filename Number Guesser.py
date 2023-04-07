e52b9b405d3e1a8030539cb14f72aeab8a0dc485
Sanyam-Vidyarthi <128597079+Sanyam-Vidyarthi@users.noreply.github.com>
2023-04-07 09:38:35 +0530

    Update Number Guesser.py

--- Number Guesser.py
+++ Number Guesser.py
@@ -4,17 +4,12 @@ num1 = 1
 print('ROBO :  Welcome to \'THE GUESSER\'')
 print('ROBO :  This game can include upto 2 players')
 print('ROBO :  In this game you have to guess a number between \'1 to 100\'')
-players = input('ROBO :  Please tell how many players will play ?  ')
-while num1 <= 100:
-    num.append(num1)
-    num1 += 1
-random = random.choice(num)
-
-
+difficulty = input('ROBO :  Which difficulty would you want to play with (Easy , Medium or Hard) ? ').lower().strip()
 def singleplayer():
     chance = 14
     while chance >= 0:
         ans = input('Guess the Number : ')
+        print('')
         if chance > 0:
             if ans == random:
                 print('You guessed it right')
@@ -27,6 +22,7 @@ def singleplayer():
         if chance == 0:
             print('You lost')
             print('You are left with 0 chances')
+            quit()
         chance -= 1
 
 
@@ -38,6 +34,7 @@ def multiplayer():
     while chance >= 0:
         if time % 2 == 0:
             player1 = input(f'{p1} Guess the Number : ')
+            print('')
             if chance > 0:
                 if player1 == random:
                     print('You guessed it right')
@@ -50,6 +47,7 @@ def multiplayer():
             chance1 -= 1
         elif time % 2 == 1:
             player2 = input(f'{p2} Guess the number :  ')
+            print('')
             if chance > 0:
                 if player2 == random:
                     print('You guessed it right')
@@ -62,9 +60,20 @@ def multiplayer():
             chance2 -= 1
         chance -= 1
         time -= 1
+        if time == 0:
+           print('you both lost')
+           print('you are left with 0 chances')
+           quit()
+            
 
 
-match players:
+def easy():
+    num1 = 1
+    while num1 <= 20:
+        num.append(num1)
+        num1 += 1
+player = input('How many players wil play ?  ')
+match player:
     case '1' | 'one' | 'One':
         name = input('Please tell your name : ')
         print(f'{name}, You will have a total of 15 chances')
@@ -74,3 +83,58 @@ match players:
         p2 = input('Please tell who will be Player 1 : ')
         print(f'{p1} and {p2}, You both will have a total of 20 chances (10 each)')
         multiplayer()
+    case _:
+        print('This game supports players upto 2 only')
+
+
+
+def medium():
+    num1 = 1
+    while num1 <= 20:
+        num.append(num1)
+        num1 += 1
+player = input('How many players wil play ?  ')
+match player:
+    case '1' | 'one' | 'One':
+        name = input('Please tell your name : ')
+        print(f'{name}, You will have a total of 15 chances')
+        singleplayer()
+    case '2' | 'two' | 'Two':
+        p1 = input('Please tell who will be Player 1 : ')
+        p2 = input('Please tell who will be Player 1 : ')
+        print(f'{p1} and {p2}, You both will have a total of 20 chances (10 each)')
+        multiplayer()
+    case _:
+        print('This game supports players upto 2 only')
+
+
+
+def hard():
+    num1 = 1
+    while num1 <= 20:
+        num.append(num1)
+        num1 += 1
+player = input('How many players wil play ?  ')
+match player:
+    case '1' | 'one' | 'One':
+        name = input('Please tell your name : ')
+        print(f'{name}, You will have a total of 15 chances')
+        singleplayer()
+    case '2' | 'two' | 'Two':
+        p1 = input('Please tell who will be Player 1 : ')
+        p2 = input('Please tell who will be Player 1 : ')
+        print(f'{p1} and {p2}, You both will have a total of 20 chances (10 each)')
+        multiplayer()
+    case _:
+        print('This game supports players upto 2 only')
+
+
+match difficulty:
+    case 'easy':
+        easy()
+    case medium():
+        medium()
+    case hard():
+        hard()
+    case _:
+        print('No such difficulty exists')
